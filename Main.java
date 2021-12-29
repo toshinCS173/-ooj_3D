30b52e53ee265c1870f81a8282e0149aefe85b3a
toshinCS173 <84076032+toshinCS173@users.noreply.github.com>
2021-12-29 12:11:07 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+class multiple<T,V>
+{
+ T ob1;
+ V ob2;
+
+ multiple(T o1, V o2)
+ {
+  ob1=o1;
+  ob2=o2;
+ }
+
+ void showTypes()
+ {
+  System.out.println("Type of first:" +ob1.getClass().getName());
+  
+ 
+  System.out.println("Type of Second:" +ob2.getClass().getName());
+ }
+ T getob1()
+ {
+  return ob1;
+ }
+ V getob2()
+ {
+  return ob2;
+ }
+}
+
+class Main
+{
+ public static void main(String args[])
+ {
+  Scanner scan=new Scanner(System.in);
+  System.out.println("Enter any string");
+  String s=scan.nextLine();
+  System.out.println("Enter any integer");
+  int i = scan.nextInt();
+
+  System.out.println();
+
+  System.out.println("Enter any string");
+  String st=scan.next();
+  System.out.println("Enter any double");
+  double d = scan.nextDouble();
+
+  multiple<String,Integer>tgObj = new multiple<String,Integer>(s,i);
+  tgObj.showTypes();
+
+  String str = tgObj.getob1();
+  System.out.println("Value of set1:"+str);
+  int v = tgObj.getob2();
+  System.out.println("Value of set1:"+v);
+
+  System.out.println();
+
+  multiple<String,Double>tObj = new multiple<String,Double>(st,d);
+  tObj.showTypes();
+
+  String strn = tObj.getob1();
+  System.out.println("Value of set2:"+strn);
+  double vd = tObj.getob2();
+  System.out.println("Value of set2:"+vd);
+ }
+}
